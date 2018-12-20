f9322352b12cc59643ab888a184dd9dd4e80e65f
Filipe Dev <italo@infomix.local>
2018-12-20 16:48:16 -0200

    Iniciando o repositorio

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  router,
+  render: h => h(App)
+});
